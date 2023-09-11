c4443ba7ce6d2f60581e2decb728bbaef2077a80
Maciej <mkuchcik@gmail.com>
2023-09-11 16:28:34 +0200

    Initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1758572.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 137081854, 137084530, 137271951, 137274521, 137277400, 137277754, 137277905, 137307062, 137307167, 138692972, 137358493, 137365620, 137365654, 137557641, 137605802, 138092625, 138175302, 138379567, 138621335, 138687060, 138687646, 138693035 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/137082393/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/137082394/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/137082392/1/ammo.js', 'preload' : true},
+];
